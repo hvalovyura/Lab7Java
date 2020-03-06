02021448bea4850f7074b8683c7464cd53cf530b
Yura <hvalovyura@gmail.com>
2020-03-07 00:55:51 +0300

    Adding part of MainFrame constructor with elements of frame without their locations

--- src/Lab7/MainFrame.java
+++ src/Lab7/MainFrame.java
@@ -1,6 +1,9 @@
 package Lab7;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame
 {
@@ -22,10 +25,51 @@ public class MainFrame extends JFrame
     private static final int SERVER_PORT = 4567;
 
     private final JTextField textFieldFrom;
-    private final JTextField textFiedTo;
+    private final JTextField textFieldTo;
 
     private final JTextArea textAreaIncoming;
     private final JTextArea textAreaOutgoing;
+
+    public MainFrame()
+    {
+        super(FRAME_TITLE);
+        setMinimumSize(new Dimension(FRAME_MINIMUM_WIDTH, FRAME_MINIMUM_HEIGHT));
+        final Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - getWidth()) / 2, kit.getScreenSize().height - getHeight() - 2);
+
+        textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
+        textAreaIncoming.setEditable(false);
+
+        final JScrollPane scrollPanelIncoming = new JScrollPane(textAreaIncoming);
+
+        final JLabel labelFrom = new JLabel("От");
+        final JLabel labelTo = new JLabel("Получатель");
+
+        textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
+        textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
+
+        textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+
+        final JScrollPane scrollPanelOutgoing = new JScrollPane(textAreaOutgoing);
+
+        final JPanel messagePanel = new JPanel();
+        messagePanel.setBorder(BorderFactory.createTitledBorder("Сообщение"));
+
+        final JButton sendButton = new JButton("Отправить");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                sendMessage();
+            }
+        });
+
+        final GroupLayout layout2 = new GroupLayout(messagePanel);
+        messagePanel.setLayout(layout2);
+
+        
+
+    }
 
     public static void main(String[] args)
     {
