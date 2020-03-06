a8df5bbacb9a0c422a64e2ab92a2e510cc16dc3f
Yura <hvalovyura@gmail.com>
2020-03-07 00:38:31 +0300

    Adding fields into MainFrame class

--- src/Lab7/MainFrame.java
+++ src/Lab7/MainFrame.java
@@ -1,8 +1,34 @@
 package Lab7;
 
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame
+{
+    private static final String FRAME_TITLE = "Клиент мгновенных сообщений";
+
+    private static final int FRAME_MINIMUM_WIDTH = 500;
+    private static final int FRAME_MINIMUM_HEIGHT = 500;
+
+    private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
+    private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
+
+    private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
+    private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+
+    private static final int SMALL_GAP = 5;
+    private static final int MEDIUM_GAP = 10;
+    private static final int LARGE_GAP = 15;
+
+    private static final int SERVER_PORT = 4567;
+
+    private final JTextField textFieldFrom;
+    private final JTextField textFiedTo;
+
+    private final JTextArea textAreaIncoming;
+    private final JTextArea textAreaOutgoing;
+
     public static void main(String[] args)
     {
-        System.out.println("qq");
+
     }
 }
